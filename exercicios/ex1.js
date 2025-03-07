abff7e3b65e287ce79279113dec6d8ba72162453
Franfran1612 <franfransilva160@gmail.com>
2025-03-07 11:00:57 -0300

    bgb

---
+++ exercicios/ex1.js
@@ -0,0 +1,82 @@
+function limparinformacao() {
+    var limpar = confirm("confirmar limpeza?")
+    if (limpar) {
+        document.getElementById("nome").value = ""
+        document.getElementById("sobrenome").value = ""
+        document.getElementById("endereço").value = ""
+        document.getElementById("bairro").value = ""
+        document.getElementById("complemento").value = ""
+        document.getElementById("cidade").value = ""
+        document.getElementById("estado").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("senha").value = ""
+
+    }
+}
+
+ function  colorn() {
+     const colorn = document.getElementById("nome")
+     colorn.style.color = "white"
+ }
+  function  colors() {
+     const colors = document.getElementById("sobrenome")
+     colors.style.color = "white"
+ }
+  function  colore() {
+     const colore = document.getElementById("endereço")
+     colore.style.color = "white"
+ }
+  function  colorb() {
+     const colorb = document.getElementById("bairro")
+     colorb.style.color = "white"
+ }
+  function  colorc() {
+     const colorc = document.getElementById("complemento")
+     colorc.style.color = "white"
+ }
+  function  colorci() {
+     const colorci = document.getElementById("cidade")
+     colorci.style.color = "white"
+ }
+  function  coloriem() {
+     const coloriem = document.getElementById("email")
+     coloriem.style.color = "white"
+ }
+  function  colorf() {
+     const colorf = document.getElementById("senha")
+     colorf.style.color = "white"
+ }
+  // ###################################################################################
+
+  function  testn() {
+     document.getElementById("nome1").value = "nome"
+
+ }
+   function  tests() {
+     document.getElementById("sobrenome1").value = "sobrenome"
+
+ }
+   function  teste() {
+     document.getElementById("endereço1").value = "endereço"
+
+ }
+   function  testb() {
+     document.getElementById("bairro1").value = "bairro"
+
+ }
+   function  testc() {
+     document.getElementById("complemento1").value = "complemento"
+
+ }
+   function  testi() {
+     document.getElementById("cidade1").value = "cidade"
+
+ }
+   function  testem() {
+     document.getElementById("senha1").value = "senha"
+
+ }
+   function  testf() {
+     document.getElementById("nome1").value = "nome"
+
+ }
---
+++ exercicios/exemplo.js
@@ -0,0 +1,31 @@
+// ele mostra na barra a mensagem no caso exibe o conteudo
+// alert("Ola mundo")
+
+// var caixa de texto
+// var text = prompt("digite qualquer coisa ")
+// alert(test)
+
+
+// function serve pra quando vc aperta aparece farias vezes a palavra
+// ESSE function  testTexto(){
+    // += aterna as palavras no caso ajunta
+    //ESSE document.getElementById("titulo").innerHTML += "Boa tarde"
+// }
+
+// ESSE function  testcolor(){
+    // += aterna as palavras no caso ajunta
+    //ESSE const titulo= document.getElementById("titulo")
+
+    //ESSE  titulo.style.color = "red"
+    //ESSE  titulo.style.fontSize = "40px"
+// }
+
+//ESSE  function  reset(){
+//  ESSE    document.getElementById("titulo").innerHTML = "Bem-vindo a Nossa Loja! "
+// }
+
+//ESSE  function testInput(){
+//ESSE      document.getElementById("input").value = "Tarde"
+// }
+
+
